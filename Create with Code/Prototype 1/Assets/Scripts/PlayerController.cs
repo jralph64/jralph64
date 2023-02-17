22cdbe80c52f867cee2a2982015a7630e5df1fb4
Joshua Ralph <122485364+jralph64@users.noreply.github.com>
2023-02-17 17:55:07 -0500

    adding input

--- Create with Code/Prototype 1/Assets/Scripts/PlayerController.cs
+++ Create with Code/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 20f;
     public float turnSpeed;
+    public float horizontalInput;
 
     // Start is called before the first frame update
     void Start()
@@ -18,8 +19,9 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame    
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
         // Move the vehicle forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
